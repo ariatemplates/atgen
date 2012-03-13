77da584368f83349ce04da389c77853d82b87055
f-bernardo <f.bernardo.it@gmail.com>
2012-03-13 10:21:06 +0100

    added some args controls

--- at gen.js
+++ at gen.js
@@ -7,7 +7,7 @@ var argv = require("optimist")
 	.string("m").alias("m", "module").describe('m', "Project module name")
 	.string("t").alias("t", "template").describe('t', "Create an html template")
 	.boolean("s").alias("s", "script").describe('s', 'Add a template script')
-	.string("w").alias("w", "wizard").describe('w', "Run the wizard with params")
+	.boolean("w").alias("w", "wizard").describe('w', "Run the wizard with params")
 	.string("c").alias("c", "controller").describe('c', "Create a controller")
 	.string("i").alias("i", "interface").describe('i', "Create an interface for the controller created")
 	.string("r").alias("r", "macro").describe('r', "Create a macro library file")
@@ -29,22 +29,34 @@ if (argv.h) {
 }
 
 var argvSize = utility.size(argv);
-var noParams = (argv.s == false && argv.b == false && argv.m == null && argvSize == 8);
+var noParams = (argv.s == false && argv.b == false && argv.m == null && argvSize == 10);
 //var noParams = (argv.s == false && argv.b == false && argv.m == null && argv.h == false && argv.t == null && argv.l == null && argv.w == null && argv.c == null && argv.i == null && argv.r == null && argv.x == null);
 
 // execute the wizard typing at gen
 if (noParams) {
-	console.log('\n\n***Welcome to the Aria Templates wizard***'.msg);
+	console.log('\n\n*** Welcome to the Aria Templates wizard ***'.msg);
 	console.log('[Info] To skip a step press Enter'.info);
-	console.log('[Info] To exit press ^C'.info);
-	
+	console.log('[Info] To exit press ^C'.info);	
 	wizard.start();
 } else {
 	if (argv.m == null || argv.m == true) {
 		console.log('\n[Error] You have to provide the module name.'.err);
 		console.log('[Info] Please check the help using -h or -help.'.info);
 		process.exit(0);
+	} else if (argv.m instanceof Array) {
+		console.log('\n[Error] You can provide the module name only once.'.err);
+		console.log('[Info] Please check the help using -h or -help.'.info);
+		process.exit(0);
+	} else if (argv.t instanceof Array || argv.c instanceof Array || argv.i instanceof Array || argv.r instanceof Array || argv.x instanceof Array) {
+		console.log('\n[Error] Duplicate args not allowed.'.err);
+		console.log('[Info] Please check the help using -h or -help.'.info);
+		process.exit(0);
+	} else if (argv.t == true || argv.c == true || argv.i == true || argv.r == true || argv.x == true) {
+		console.log('\n[Error] You can\'t use a string argument without specify any content.'.err);
+		console.log('[Info] Please check the help using -h or -help.'.info);
+		process.exit(0);
 	} else {
+		console.log(argv);
 		paramsMode.execute(argv);
 	}	
 }
