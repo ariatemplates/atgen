40290b3e98bbdd99c69baa0e752a18cf7542771c
f-bernardo <f.bernardo.it@gmail.com>
2012-03-16 16:32:23 +0100

    config handler added

--- atgen.js
+++ atgen.js
@@ -3,7 +3,7 @@
 
 // module to manage the command line arguments
 var argv = require("optimist")
-    .usage('Aria Templates wizard tool')
+    .usage('\n\nAria Templates wizard tool\n\nUsage: atgen [options]')
 	.string("t").alias("t", "template").describe('t', "Create an html template")
 	.boolean("s").alias("s", "script").describe('s', 'Add a template script')
 	.boolean("w").alias("w", "wizard").describe('w', "Run the wizard with params")
@@ -19,7 +19,8 @@ var argv = require("optimist")
 // module to manage the wizard mode
 var wizard = require("./lib/wizard"),
 	   utility = require("./lib/utility"),
-	   paramsMode = require("./lib/paramsMode");
+	   paramsMode = require("./lib/paramsMode"),
+	   configHandler = require("./lib/configHandler");
 	   
 // show the help
 if (argv.h) {
@@ -27,33 +28,41 @@ if (argv.h) {
 	process.exit(0);
 }
 
-var argvSize = utility.size(argv);
-var noParams = (argv.s == false && argv.b == false && argv.m == null && argvSize == 10);
-
-// execute the wizard typing at gen
-if (noParams) {
-	console.log('\n\n*** Welcome to the Aria Templates wizard ***'.msg);
-	console.log('[Info] To skip a step press Enter'.info);
-	console.log('[Info] To exit press ^C'.info);	
-	wizard.start();
+if (configHandler.checkConfigFile()) {
+	runTool();
 } else {
-	if (argv._ == null || argv._ == "") {
-		console.log('\n[Error] You have to provide the module name.'.err);
-		console.log('[Info] Please check the help using -h or -help.'.info);
-		process.exit(0);
-	} else if (argv._.length > 1) {
-		console.log('\n[Error] You can provide the module name only once.'.err);
-		console.log('[Info] Please check the help using -h or -help.'.info);
-		process.exit(0);
-	} else if (argv.t instanceof Array || argv.c instanceof Array || argv.i instanceof Array || argv.r instanceof Array || argv.x instanceof Array) {
-		console.log('\n[Error] Duplicate args not allowed.'.err);
-		console.log('[Info] Please check the help using -h or -help.'.info);
-		process.exit(0);
-	} else if (argv.t == true || argv.c == true || argv.i == true || argv.r == true || argv.x == true) {
-		console.log('\n[Error] You can\'t use a string argument without specify any content.'.err);
-		console.log('[Info] Please check the help using -h or -help.'.info);
-		process.exit(0);
-	} else {
-		paramsMode.execute(argv);
-	}	
+	configHandler.createConfigFile();
 }
+
+function runTool() {
+	var argvSize = utility.size(argv);
+	var noParams = (argv.s == false && argv.b == false && argv.m == null && argvSize == 10);
+	
+	// enter in wizard mode (typing at gen )
+	if (noParams) {
+		console.log('\n\n*** Welcome to the Aria Templates wizard ***'.msg);
+		console.log('[Info] To skip a step press Enter'.info);
+		console.log('[Info] To exit press ^C'.info);	
+		wizard.start();
+	} else {	// enter in the Command line mode
+		if (argv._ == null || argv._ == "") {
+			console.log('\n[Error] You have to provide the module name.'.err);
+			console.log('[Info] Please check the help using -h or -help.'.info);
+			process.exit(0);
+		} else if (argv._.length > 1) {
+			console.log('\n[Error] You can provide the module name only once.'.err);
+			console.log('[Info] Please check the help using -h or -help.'.info);
+			process.exit(0);
+		} else if (argv.t instanceof Array || argv.c instanceof Array || argv.i instanceof Array || argv.r instanceof Array || argv.x instanceof Array) {
+			console.log('\n[Error] Duplicate args not allowed.'.err);
+			console.log('[Info] Please check the help using -h or -help.'.info);
+			process.exit(0);
+		} else if (argv.t == true || argv.c == true || argv.i == true || argv.r == true || argv.x == true) {
+			console.log('\n[Error] You can\'t use a string argument without specify any content.'.err);
+			console.log('[Info] Please check the help using -h or -help.'.info);
+			process.exit(0);
+		} else {
+			paramsMode.execute(argv);
+		}	
+	}
+};
